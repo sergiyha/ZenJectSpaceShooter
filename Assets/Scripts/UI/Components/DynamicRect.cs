34e52870e1346bceeb07073c26f8e8570796dd91
Sergey <s.galanov@room8studio.com>
2018-07-20 10:30:43 +0300

    - add dynamic rect

---
+++ Assets/Scripts/UI/Components/DynamicRect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Components
+{
+	[RequireComponent(typeof(RectTransform))]
+	public class DynamicRect : MonoBehaviour, IBaseView<DynamicRect.DynamicRectSettings>
+	{
+		public event Action OnFinished;
+
+		private RectTransform _bodyRect;
+
+
+
+		public void Init(DynamicRectSettings payload = null)
+		{
+			_bodyRect = _bodyRect ?? GetComponent<RectTransform>();
+		}
+
+
+		public class DynamicRectSettings
+		{
+		}
+	}
+}
